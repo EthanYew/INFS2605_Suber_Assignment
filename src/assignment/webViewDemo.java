4c0c540db812f11272224abfe0095af0767e4faf
EthanYew <ethan.wei.han.yew@gmail.com>
2017-05-17 02:16:27 +1000

    experimenting with source code found online for google maps http://developers-club.com/posts/170141/

---
+++ src/assignment/webViewDemo.java
@@ -0,0 +1,43 @@
+package assignment;
+
+import java.net.URL;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
+/**
+*
+* @web http://java-buddy.blogspot.com/
+*/
+public class webViewDemo extends Application {
+
+/**
+ * @param args the command line arguments
+ */
+public static void main(String[] args) {
+    launch(args);
+}
+
+@Override
+public void start(Stage primaryStage) {
+    primaryStage.setTitle("java-buddy.blogspot.com");
+
+    WebView browser = new WebView();
+    WebEngine engine = browser.getEngine();
+    String url = Assignment.class.getResource("/assignment/map.html").toExternalForm();
+    engine.load(url);
+    
+    StackPane sp = new StackPane();
+    sp.getChildren().add(browser);
+    Scene root = new Scene(sp);
+
+    primaryStage.setScene(root);
+    primaryStage.show();
+}
+
+
+}

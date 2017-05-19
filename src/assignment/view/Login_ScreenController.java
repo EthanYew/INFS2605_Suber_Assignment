d76f72f4e1d55dee81086bd95a9e2e1bb9efc14d
EthanYew <ethan.wei.han.yew@gmail.com>
2017-05-19 18:55:34 +1000

    test

--- src/assignment/view/Login_ScreenController.java
+++ src/assignment/view/Login_ScreenController.java
@@ -5,17 +5,22 @@
  */
 package assignment.view;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -44,7 +49,17 @@ public class Login_ScreenController implements Initializable {
     }    
 
     @FXML
-    private void SignUpButton(ActionEvent event) {
+    private void SignUpButton(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+       
+        stage=(Stage) SignUp.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Home_Page.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+    
+    
     
 }

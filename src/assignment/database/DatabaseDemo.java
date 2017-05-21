90c73b48d8574bff5bffb4eb963ec3754dbe48b2
EthanYew <ethan.wei.han.yew@gmail.com>
2017-05-21 21:14:49 +1000

    starting afresh again, ajay and ari will work on frontend, ethan will work on db

--- src/assignment/database/DatabaseDemo.java
+++ src/assignment/database/DatabaseDemo.java
@@ -26,24 +26,28 @@ public class DatabaseDemo {
         //create and insert for student table
         //stmt.execute("CREATE TABLE student(name VARCHAR(50), wam DOUBLE);");
         
-
-        
         //gives object that u run queries against, 
         //statement is not actual sql statement but u put sql statement in here and it runs it
-        //3 kinds of statements: execute, executeUpdate, executeQuery
+        
+//3 kinds of statements: execute, executeUpdate, executeQuery
         //create - execute
         //update - executeUpdate
         //get something from table - executeQuery
+        
         //SQL grammar speccific to derby or h2
         //some differences between oracle and h2 
+        
         //1. Create table
         //stmt.execute("CREATE TABLE SampleTable(cars VARCHAR(50), profession VARCHAR(50))");
         //once create tbale statement run comment out
+        
         //2. Insert values into table
 //        stmt.execute("INSERT INTO SampleTable VALUES('Car', 'Job');");
+
         //3. Update table
 //        int n = stmt.executeUpdate("UPDATE sampleTable SET cars = 'Ferrari' WHERE profession = 'Job'");
 //        System.out.println(n);
+       
         //executeUpdate tells  u how many rows have been updated
         //when u query something - u get back a resultset
         ResultSet result = stmt.executeQuery("SELECT * FROM sampleTable");
--- src/assignment/view/mapRoute.java
+++ src/assignment/view/mapRoute.java
@@ -9,8 +9,13 @@ import assignment.Assignment;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -21,6 +26,9 @@ import javafx.stage.Stage;
  * @author EthanYew
  */
 public class mapRoute extends Application {
+    
+    private String originPlaceId;
+    private String destinationPlaceId;
 
     @FXML
     private WebView webView;
@@ -38,12 +46,53 @@ public class mapRoute extends Application {
         WebView webView = new WebView();
         WebEngine engine = webView.getEngine();
         String url = Assignment.class.getResource("/assignment/placesAutocompleteDirections.html").toExternalForm();
+        
+        //engine.setCreatePopupHandler(null);
         engine.load(url);
+        
+        Button btn_saveOriginPlaceId = new Button();
+        btn_saveOriginPlaceId.setText("Save Origin PlaceId");
+        btn_saveOriginPlaceId.setOnAction(new EventHandler<ActionEvent>() {        
+            @Override
+            public void handle(ActionEvent event) {
 
-        StackPane sp = new StackPane();
-        sp.getChildren().add(webView);
-        Scene root = new Scene(sp);
+               //Pass this to javascript
+//               engine.executeScript("myName("+"'"+ name +"'"+ ")");
 
+               //get return value from javascript
+               originPlaceId = (String)engine.executeScript("returnOriginPlaceId()");
+               System.out.println(originPlaceId);
+
+               
+            }
+            });
+        
+        Button btn_saveDestinationPlaceId = new Button();
+        btn_saveDestinationPlaceId.setText("Save Destination PlaceId");
+        btn_saveDestinationPlaceId.setOnAction(new EventHandler<ActionEvent>() {        
+            @Override
+            public void handle(ActionEvent event) {
+
+               //Pass this to javascript
+//               engine.executeScript("myName("+"'"+ name +"'"+ ")");
+
+               //get return value from javascript
+               destinationPlaceId = (String)engine.executeScript("returnDestinationPlaceId()");
+               System.out.println(destinationPlaceId);
+
+               
+            }
+            });
+        
+        BorderPane bp = new BorderPane();
+        bp.setCenter(webView);
+        Scene root = new Scene(bp);
+        
+        FlowPane fp = new FlowPane();
+        fp.getChildren().addAll(btn_saveOriginPlaceId, btn_saveDestinationPlaceId);
+        
+        bp.setBottom(fp);
+        
         primaryStage.setScene(root);
         primaryStage.show();
 
@@ -51,6 +100,7 @@ public class mapRoute extends Application {
 
     public void initialize(URL url, ResourceBundle rb) {
         engine = webView.getEngine();
+        //engine.setCreatePopupHandler(null);
         engine.load(Assignment.class.getResource("/assignment/placesAutocompleteDirections.html").toExternalForm());
     }
 
